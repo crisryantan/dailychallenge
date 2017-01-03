7d25a89d027705091ce4dde0fef77360f8f9ba84
Cris Ryan Tan <crisryantan@gmail.com>
2017-01-03 14:38:02 +0800

    use es6 when using event emitter

---
+++ 2017/January/2/use-node-own-event-emitter/app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var Greetr  = require( './greetr' );
+var greeter = new Greetr();
+
+greeter.on( 'greet', function ( data ) {
+	console.log( 'greet has been triggered' + ': ' + data )
+} )
+
+greeter.greet( 'Ryan' );
+
+// old code - not es6
+// var util         = require( 'util' );
+
+// function Greetr () {
+// 	// Ensures that my object created from this function constructor has everything
+// 	// in object created from an event emitter
+// 	EventEmitter.call( this );
+// 	this.greeting = 'Hello World';
+// }
+
+// util.inherits( Greetr, EventEmitter );
+
+// Greetr.prototype.greet = function ( data ) {
+// 	console.log( this.greeting + ': ' + data );
+
+// 	// since we inherit everything from EventEmitter we could use the emit method.
+// 	this.emit( 'greet', data );
+// }
