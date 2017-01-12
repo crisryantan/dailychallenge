f8d753e652ad3497dc3721d5ec1c7337bc5e74d2
Cris Ryan Tan <crisryantan@gmail.com>
2017-01-12 15:28:19 +0800

    added add inventory feature
    - also added component for displaying all the added inventory

--- 2017/January/learn-react/catch-of-the-day/src/components/App.js
+++ 2017/January/learn-react/catch-of-the-day/src/components/App.js
@@ -1,17 +1,63 @@
 import React from 'react';
 import Header from './Header';
 import Inventory from './Inventory';
+import Fish from './Fish';
 import Order from './Order';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
+
+	constructor () {
+		super();
+
+		this.addFish     = this.addFish.bind( this );
+		this.loadSamples = this.loadSamples.bind( this );
+		// initial state
+		this.state = {
+			'fishes' : {},
+			'order'  : {}
+		}
+	}
+
+	addFish ( fish ) {
+		// update state
+		const fishes = { ...this.state.fishes };
+		// add new fish
+		const timestamp = Date.now();
+		fishes[ `fish-${timestamp}` ] = fish;
+		// set state
+		this.setState( { fishes } );
+	}
+
+	loadSamples () {
+		this.setState( {
+			fishes : sampleFishes
+		} );
+	}
+
 	render () {
 		return (
 			<div className="catch-of-the-day">
 				<div className="menu">
 					<Header tagline="Fresh Seafood Market" />
+					<ul className="list-of-fishes">
+						{
+							Object
+								.keys( this.state.fishes )
+								.map( key => 
+									<Fish 
+										key={key}
+										details={this.state.fishes[ key ]}
+									/>
+								)
+						}
+					</ul>
 				</div>
 				<Order />
-				<Inventory />
+				<Inventory
+					loadSamples={ this.loadSamples }
+					addFish={ this.addFish }
+				/>
 			</div>
 		)
 	}
