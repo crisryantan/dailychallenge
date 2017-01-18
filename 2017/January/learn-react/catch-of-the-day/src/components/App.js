ef73602103463913c565608cd6b735df357dddb7
Cris Ryan Tan <crisryantan@gmail.com>
2017-01-18 14:31:52 +0800

    add component validation

--- 2017/January/learn-react/catch-of-the-day/src/components/App.js
+++ 2017/January/learn-react/catch-of-the-day/src/components/App.js
@@ -102,7 +102,7 @@ class App extends React.Component {
 		return (
 			<div className="catch-of-the-day">
 				<div className="menu">
-					<Header tagline="Fresh Seafood Market" />
+					<Header tagline={"Fresh Seafood Market"} />
 					<ul className="list-of-fishes">
 						{
 							Object
@@ -137,5 +137,9 @@ class App extends React.Component {
 		)
 	}
 }
+
+App.propTypes =  {
+	params : React.PropTypes.object.isRequired
+};
 
 export default App;
